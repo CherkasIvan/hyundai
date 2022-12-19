06529cbba80bfdc03956fc0f32e1516d36c3dbaa
ivan.cherkas <ivan.cherkas@intexsoft.by>
2022-12-19 10:51:40 +0300

    create modal without formly

--- src/app/pages/main-form-content/componets/processing-page/components/insurance-polices-modal-body/insurance-polices-modal-body.component.ts
+++ src/app/pages/main-form-content/componets/processing-page/components/insurance-polices-modal-body/insurance-polices-modal-body.component.ts
@@ -10,56 +10,60 @@ export class InsurancePolicesModalBodyComponent implements OnInit {
     {
       img: 'ingostrah.png',
       status: 'green',
-      kasko: {
-        label: 'КАСКО',
-        checkbox: true,
-        description: 'dd',
-        price: '51 000 ₽',
-      },
-      osago: {
-        label: 'ОСАГО',
-        checkbox: false,
-        description: 'dd',
-        price: '13 000 ₽',
-      },
+      insurences: [
+        {
+          label: 'КАСКО',
+          checkbox: true,
+          description: 'dd',
+          price: '51 000 ₽',
+        },
+        {
+          label: 'ОСАГО',
+          checkbox: false,
+          description: 'dd',
+          price: '13 000 ₽',
+        },
+      ],
     },
     {
       img: 'sovkom_strah.png',
-      status: 'yeelow',
-      kasko: {
-        label: 'КАСКО',
-        checkbox: true,
-        description: 'dd',
-        price: '51 000 ₽',
-      },
-      osago: {
-        label: 'ОСАГО',
-        checkbox: false,
-        description: 'dd',
-        price: '13 000 ₽',
-      },
+      status: 'yellow',
+      insurences: [
+        {
+          label: 'КАСКО',
+          checkbox: true,
+          description: 'dd',
+          price: '51 000 ₽',
+        },
+        {
+          label: 'ОСАГО',
+          checkbox: false,
+          description: 'dd',
+          price: '13 000 ₽',
+        },
+      ],
     },
     {
       img: 'vsk.png',
-      status: 'yeelow',
-      kasko: {
-        label: 'КАСКО',
-        checkbox: true,
-        description: 'dd',
-        price: '51 000 ₽',
-      },
-      osago: {
-        label: 'ОСАГО',
-        checkbox: false,
-        description: 'dd',
-        price: '13 000 ₽',
-      },
+      status: 'yellow',
+      insurences: [
+        {
+          label: 'КАСКО',
+          checkbox: true,
+          description: 'dd',
+          price: '51 000 ₽',
+        },
+        {
+          label: 'ОСАГО',
+          checkbox: false,
+          description: 'dd',
+          price: '13 000 ₽',
+        },
+      ],
     },
   ];
 
   constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.cardsObjects);
-  }
+  ngOnInit(): void {}
 }
--- src/app/pages/main-form-content/componets/processing-page/components/insurance-polices-modal-card/insurance-polices-modal-card.component.ts
+++ src/app/pages/main-form-content/componets/processing-page/components/insurance-polices-modal-card/insurance-polices-modal-card.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormlyFormOptions, FormlyFieldConfig} from '@ngx-formly/core';
 
 @Component({
   selector: 'mc-insurance-polices-modal-card',
@@ -7,9 +9,40 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class InsurancePolicesModalCardComponent implements OnInit {
   @Input() public insuranceCompany!: any;
+
+  public formModal = new FormGroup({});
+  public model: Object = {};
+  public options: FormlyFormOptions = {};
+  public fields: FormlyFieldConfig[] = [
+    {
+      className: 'card-content__form--checkbox',
+      key: 'Checkbox',
+      type: 'checkbox',
+      templateOptions: {
+        label: this.insuranceCompany,
+      },
+      // validation: {
+      //   messages: {
+      //     pattern: 'Please accept the terms',
+      //   },
+      // },
+    },
+  ];
+
   constructor() {}
 
+  // public setLabel(): void {
+  //   this.insuranceCompany.insurences.forEach((elementIns: any) => {
+  //     console.log(elementIns.label);
+
+  //     this.fields.forEach((element: any) => {
+  //       element.templateOptions.label = elementIns.label;
+  //       console.log(element.templateOptions.label);
+  //     });
+  //   });
+  // }
+
   ngOnInit(): void {
-    console.log(this.insuranceCompany);
+    // this.setLabel();
   }
 }
