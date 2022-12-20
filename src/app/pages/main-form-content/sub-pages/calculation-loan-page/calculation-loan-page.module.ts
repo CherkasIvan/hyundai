3fe17089e4301ec4e640a76cf98dac44f69aac87
ivan.cherkas <ivan.cherkas@intexsoft.by>
2022-12-20 11:25:40 +0300

    fix arch v3

--- src/app/pages/main-form-content/sub-pages/calculation-loan-page/calculation-loan-page.module.ts
+++ src/app/pages/main-form-content/sub-pages/calculation-loan-page/calculation-loan-page.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { CalculationLoanPageRoutingModule } from './calculation-loan-page-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CalculationLoanPageRoutingModule, CommonModule],
 })
-export class CalculationLoanPageModule { }
+export class CalculationLoanPageModule {}
--- src/app/pages/main-form-content/sub-pages/processing-page/processing-page.module.ts
+++ src/app/pages/main-form-content/sub-pages/processing-page/processing-page.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ProcessingPageRoutingModule } from './processing-page-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ProcessingPageRoutingModule, CommonModule],
 })
-export class ProcessingPageModule { }
+export class ProcessingPageModule {}
