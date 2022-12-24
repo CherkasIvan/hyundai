96f090b02d9e082362939a67610cb12dfc042de0
ivan.cherkas <ivan.cherkas@intexsoft.by>
2022-12-24 09:05:05 +0300

    fix page

--- src/app/shared/global-url-store/global-url.selectors.ts
+++ src/app/shared/global-url-store/global-url.selectors.ts
@@ -1,5 +1,5 @@
-import {getSelectors, RouterReducerState} from '@ngrx/router-store';
-import {createFeatureSelector} from '@ngrx/store';
+import { getSelectors, RouterReducerState } from '@ngrx/router-store';
+import { createFeatureSelector } from '@ngrx/store';
 
 export const selectRouter = createFeatureSelector<RouterReducerState>('router');
 
@@ -7,9 +7,7 @@ export const {
   selectCurrentRoute, // select the current route
   selectFragment, // select the current route fragment
   selectQueryParams, // select the current route query params
-  selectQueryParam, // factory function to select a query param
   selectRouteParams, // select the current route params
-  selectRouteParam, // factory function to select a route param
   selectRouteData, // select the current route data
   selectUrl, // select the current url
 } = getSelectors(selectRouter);
