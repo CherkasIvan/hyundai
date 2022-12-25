e2cf4bd21488be72534a7360e66b00e191b3c615
ivan.cherkas <ivan.cherkas@intexsoft.by>
2022-12-25 11:08:08 +0300

    created routing enum not fixed

---
+++ src/app/shared/consts/path-enums.ts
@@ -0,0 +1,20 @@
+export const enum routingPath {
+  DealerAuthentication = 'auth-broker',
+  ClientAuthentication = 'auth0client',
+  MainPage = 'main-form',
+  LoanCalculation = 'loan-calculation',
+  Processing = 'processing',
+  DocumentsAndPayments = 'documents-payments',
+  CarOptions = 'car-options',
+  CarOwner = 'car-owner',
+  Drivers = 'drivers',
+  CreditOptions = 'credit-options',
+  InsurancePoliciesOptions = 'insurance-options',
+  ProductsCalculation = 'products-calculation',
+  PersonalData = 'personal-data',
+  ClientJobData = 'client-job',
+  Summary = 'summary',
+  approval = 'approval',
+}
+
+// ПОФИКСИТЬ ЭНАМ РОУТИНГОВ НА АКТУАЛЬНЫЙ!
