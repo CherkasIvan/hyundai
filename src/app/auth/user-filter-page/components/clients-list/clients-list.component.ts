5df73506dabe75eb7085f252502d71df23d203f8
ivan.cherkas <ivan.cherkas@intexsoft.by>
2023-01-03 14:58:21 +0300

    add pgination for filter page

--- src/app/auth/user-filter-page/components/clients-list/clients-list.component.ts
+++ src/app/auth/user-filter-page/components/clients-list/clients-list.component.ts
@@ -1,6 +1,7 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { BehaviorSubject } from 'rxjs';
@@ -14,7 +15,7 @@ import { GetUsersService } from '../../services/get-users.service';
 export class ClientsListComponent implements OnInit, AfterViewInit {
   public dataSource: MatTableDataSource<any> = new MatTableDataSource();
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('paginator') paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   public displayedColumns: string[] = [
@@ -29,18 +30,35 @@ export class ClientsListComponent implements OnInit, AfterViewInit {
   ];
 
   public allClints$: BehaviorSubject<any> = new BehaviorSubject([]);
-  constructor(private _getUsers: GetUsersService) {}
+  constructor(
+    private _getUsers: GetUsersService,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
+
+  public announceSortChange(sortState: any) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 
   public ngOnInit(): void {
     this._getUsers.getClients().subscribe((el) => {
       this.allClints$.next(el.clients);
-      this.dataSource = el.clients;
+      this.dataSource.data = el.clients;
     });
   }
 
   public ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    setTimeout(() => (this.dataSource.paginator = this.paginator));
+    console.log(this.paginator);
+    console.log(this.dataSource);
+    setTimeout(() => (this.dataSource.sort = this.sort));
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
