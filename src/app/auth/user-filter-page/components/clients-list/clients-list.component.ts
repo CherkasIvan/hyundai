4cb8d408b02f14d8b9d768bf4745ec478e8ca45d
ivan.cherkas <ivan.cherkas@intexsoft.by>
2023-01-03 15:20:50 +0300

    start to create sort

--- src/app/auth/user-filter-page/components/clients-list/clients-list.component.ts
+++ src/app/auth/user-filter-page/components/clients-list/clients-list.component.ts
@@ -1,10 +1,11 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-import { BehaviorSubject } from 'rxjs';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
 import { GetUsersService } from '../../services/get-users.service';
 
 @Component({
@@ -15,8 +16,8 @@ import { GetUsersService } from '../../services/get-users.service';
 export class ClientsListComponent implements OnInit, AfterViewInit {
   public dataSource: MatTableDataSource<any> = new MatTableDataSource();
 
-  @ViewChild('paginator') paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
   public displayedColumns: string[] = [
     'ФИО',
@@ -29,17 +30,13 @@ export class ClientsListComponent implements OnInit, AfterViewInit {
     'Телефон',
   ];
 
-  public allClints$: BehaviorSubject<any> = new BehaviorSubject([]);
   constructor(
     private _getUsers: GetUsersService,
     private _liveAnnouncer: LiveAnnouncer
   ) {}
 
-  public announceSortChange(sortState: any) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
+  public announceSortChange(sortState: Sort) {
+    console.log(sortState);
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
@@ -49,15 +46,12 @@ export class ClientsListComponent implements OnInit, AfterViewInit {
 
   public ngOnInit(): void {
     this._getUsers.getClients().subscribe((el) => {
-      this.allClints$.next(el.clients);
       this.dataSource.data = el.clients;
     });
   }
 
   public ngAfterViewInit() {
     setTimeout(() => (this.dataSource.paginator = this.paginator));
-    console.log(this.paginator);
-    console.log(this.dataSource);
     setTimeout(() => (this.dataSource.sort = this.sort));
 
     if (this.dataSource.paginator) {
