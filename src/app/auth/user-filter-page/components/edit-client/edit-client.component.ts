d64f66bec27b8a3d53622459ca3d6b9450b14592
ivan.cherkas <ivan.cherkas@intexsoft.by>
2023-01-10 09:27:49 +0300

    fix some checkbox

--- src/app/auth/user-filter-page/components/edit-client/edit-client.component.ts
+++ src/app/auth/user-filter-page/components/edit-client/edit-client.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 import { forkJoin, Subscription } from 'rxjs';
+import { routingPathEnum } from '../../../../shared/consts/routing-path-enum';
 
 import { CommonDataService } from '../../../../shared/services/common-data.service';
 import { PersistenceService } from '../../../../shared/services/persistence.service';
 import { ClientAuthService } from '../../services/client-auth.service';
+import { AddUserModalComponent } from '../add-user-modal/add-user-modal.component';
 
 @Component({
   selector: 'tes-edit-client',
@@ -24,25 +28,37 @@ export class EditClientComponent implements OnInit, OnDestroy {
       patronymic: ['', Validators.required],
       email: ['', Validators.required],
       clientId: ['', Validators.required],
+      checkbox: false,
     });
   }
   constructor(
     private _fb: FormBuilder,
     private _persistenceService: PersistenceService,
     private _commonDataService: CommonDataService,
-    private _clientAuthService: ClientAuthService
+    private _clientAuthService: ClientAuthService,
+    private _router: Router,
+    private _dialogRef: MatDialogRef<AddUserModalComponent>
   ) {}
 
   public sendClientData(formData: FormGroup) {
+    const isRedirect = !!this.editClientForm.get('checkbox')?.value;
     const body = formData.value;
     body.clientId = this.editClientForm.get('clientId')?.value;
 
+    delete body.checkbox;
     this._persistenceService.set('clientId', body.clientId);
+    this._commonDataService.editClientsDetails(body).subscribe((el) => {
+      el && isRedirect
+        ? this._router.navigateByUrl(
+            `/${routingPathEnum.MainPage}/${routingPathEnum.LoanCalculationPage}/${routingPathEnum.CarInfo}`
+          )
+        : this.closeDialog();
+    });
+  }
 
-    forkJoin({
-      requestOne: this._commonDataService.editClientsDetails(body),
-      requestTwo: this._clientAuthService.getClients(),
-    }).subscribe();
+  private closeDialog() {
+    this._dialogRef.close();
+    // this._getUsersService.getClients().subscribe();
   }
 
   private initializeValues(): void {
